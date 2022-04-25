bee67c317f5b3b6422979b7e70a7f5d217ed9144
Rafael <rafaeltavares462@gmail.com>
2022-04-25 19:56:00 -0300

    Gerando a views inicial da pagina

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace Inicial.Controllers
-{
-    public class HomeController : Controller
+{   // Todo controller precisa erdar a classe controller
+    public class HomeController : Controller    // tambel tem que estar na pasta controller
     {
         // GET: Home
         public ActionResult Index()
         {
+
+            // toda Action retorna a regra de nogocio e develve um vizualização 
             return View();
         }
     }
